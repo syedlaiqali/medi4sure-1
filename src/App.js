8e6ab2975d43e91a75f243b6a30a1e8ffc5d8b0b
PickupBiz <pickupyourbiz@gmail.com>
2021-09-29 12:48:22 +0530

    PS2 - Cleanup completed
    PS2 - As UI Dev I should cleanup the default code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,3 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with PickupBiz
-        </a>
-        <p>This is PickupBiz session on React App building</p>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export const App = () => {
+  return <div>Medi4Sure</div>;
+};
