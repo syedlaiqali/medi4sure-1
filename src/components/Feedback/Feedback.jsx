f7e177b1a25acbcb51ee0123786b31cf1a2c202a
PickupBiz <pickupyourbiz@gmail.com>
2021-09-29 18:53:03 +0530

    Updated code

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,10 @@
+import { Row, Col } from "react-bootstrap";
+
+export const Feedback = () => {
+  return (
+    <Row>
+      <Col lg={8}>Amar</Col>
+      <Col lg={4}>Prem</Col>
+    </Row>
+  );
+};
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,10 @@
 import { MediUpdates } from "./MediUpdates";
+import { Feedback } from "../Feedback/Feedback";
 
 export const Home = () => {
   return (
     <div>
+      <Feedback />
       <MediUpdates />
     </div>
   );
