63775ed4babaf419069f5aa0f67dfac6eb05c0fa
PickupBiz <pickupyourbiz@gmail.com>
2021-09-30 12:34:38 +0530

    Updated code checked in

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,10 +1,28 @@
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Form, Button } from "react-bootstrap";
+import "./Feedback.css";
 
 export const Feedback = () => {
   return (
-    <Row>
-      <Col lg={8}>Amar</Col>
-      <Col lg={4}>Prem</Col>
-    </Row>
+    <Form>
+      <Form.Control
+        placeholder="Enter the Title"
+        className="fbrow"
+      ></Form.Control>
+      <Form.Control
+        as="textarea"
+        placeholder="Enter Feedback here..."
+        className="fbrow"
+      ></Form.Control>
+      <Row className="fbrow">
+        <Col lg={8}></Col>
+        <Col lg={1}>
+          <Button>Submit</Button>
+        </Col>
+        <Col lg={1}>
+          <Button>Cancel</Button>
+        </Col>
+        <Col lg={2}></Col>
+      </Row>
+    </Form>
   );
 };
