59320209237bf0414a0fd6dc9fa7b421e2246496
PickupBiz <pickupyourbiz@gmail.com>
2021-09-30 18:31:19 +0530

    Updated EMI Calc

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import { MediUpdates } from "./MediUpdates";
 import { Feedback } from "../Feedback/Feedback";
+import { EMICalc } from "../EMICalc/EMICalc";
 
 export const Home = () => {
   return (
     <div>
+      <EMICalc />
       <Feedback />
       <MediUpdates />
     </div>
