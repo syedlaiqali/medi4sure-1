396336166e6884b663b8fd4ecc12160d1bbb16d3
PickupBiz <pickupyourbiz@gmail.com>
2021-10-01 12:49:12 +0530

    Updated Font Awesome

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,16 @@
 import { MediUpdates } from "./MediUpdates";
 import { Feedback } from "../Feedback/Feedback";
 import { EMICalc } from "../EMICalc/EMICalc";
+import { HeaderForComp } from "../Common/HeaderForComp";
 
 export const Home = () => {
   return (
     <div>
+      <HeaderForComp title="EMI Calculator" />
       <EMICalc />
+      <HeaderForComp title="Feedback" />
       <Feedback />
+      <HeaderForComp title="Medicare Updates" />
       <MediUpdates />
     </div>
   );
